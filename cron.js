df5e674427a4dfd4982c720a3e10f00df9f2151a
Taha Shahid <tahashahid@192.168.10.11>
2022-04-12 22:23:57 +0500

    cron job task done

--- cron.js
+++ cron.js
@@ -2,8 +2,8 @@ const cron = require('node-cron');
 
 cron.schedule('* * * * *', () => {
     console.log('running a task every minute');
-});
+}).start();
 
 cron.schedule('* * * * *', () => {
     console.log('running a task every minute by taha');
-});
+}).start();
